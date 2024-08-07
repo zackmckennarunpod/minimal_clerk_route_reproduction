177dc8f4383e5b7bc177946b604bd0410afe84d0
Zachary McKenna <zack.mckenna@runpod.io>
2024-08-07 11:01:29 -0400

    remove additional middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
 
 export const config = {
   matcher: "/((?!.*\\.).*)",
@@ -12,7 +13,29 @@ export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   try {
+    // Check whether the maintenance page should be shown
+    const isInMaintenanceMode = await get<boolean>("isInMaintenanceMode");
+    const hiddenLinks: string[] = (await get<string[]>("hiddenLinks")) || [];
+    const disabledRoutes: string[] =
+      (await get<string[]>("disabledRoutes")) || [];
+    const featureFlags: string[] = (await get<string[]>("featureFlags")) || [];
+    const banners: string[] = (await get<string[]>("banners")) || [];
     const res = NextResponse.next();
+    res.cookies.set("hiddenLinks", JSON.stringify(hiddenLinks));
+    res.cookies.set("disabledRoutes", JSON.stringify(disabledRoutes));
+    res.cookies.set("featureFlags", JSON.stringify(featureFlags));
+    res.cookies.set("banners", JSON.stringify(banners));
+
+    // If is in maintenance mode, point the url pathname to the maintenance page
+    if (isInMaintenanceMode) {
+      // redirect to www maintenance page
+      return NextResponse.redirect(`${req.nextUrl?.origin}/maintenance`);
+    }
+
+    if (disabledRoutes.some((route) => pathname.startsWith(route))) {
+      const redirect = NextResponse.redirect(`${req.nextUrl?.origin}/404`);
+      return redirect;
+    }
 
     // If nothing matches, just continue with the request and set the cookie
     return res;
